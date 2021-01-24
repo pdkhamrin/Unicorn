48c4064a3bb6a46bdd67426daee6a8f05c596fe0
pdkhamrin <57846789+pdkhamrin@users.noreply.github.com>
2021-01-24 23:00:11 +0300

    Update MetadataDDL.sql

--- MetadataDDL.sql
+++ MetadataDDL.sql
@@ -92,7 +92,8 @@ insert into metadata.attr_type
 values 
 ('table_name','Наименование таблицы','table')
 ,('table_type','Тип таблицы','table')
-,('schema','Схема таблицы','table');
+,('schema','Схема таблицы','table')
+,('source_table_name','Наименование таблицы на источнике','table');
 
 
 create table metadata.source_table
